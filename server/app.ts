8e923db1ef2a68251a2e8c22f278f5f6d3bd0827
natashaamin <natashasiaamin@gmail.com>
2023-05-07 16:45:48 +0800

    deploy

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
+import path from "path";
 import defaultRoute from './routes/route';
 import { errorHandler } from "./services/service";
 
@@ -18,6 +19,13 @@ routes.use(defaultRoute)
 app.use('/', routes);
 
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(path.resolve(), "dist")));
+} else {
+  app.use("/", express.static(path.join(path.resolve(), "public")));
+  app.use("/server", routes);
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
